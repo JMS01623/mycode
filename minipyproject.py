d277a65652e5d144a3b6755527471220a2d9d319
JMS01623 <michaeljustinc@gmail.com>
2023-08-16 22:30:21 +0000

    Food choices

---
+++ minipyproject.py
@@ -0,0 +1,13 @@
+food = input("Choose your choice for food: Chicken, Noodles, Soup, Beef\n")
+if (food == "chicken") or (food =="Chicken"):
+    print("Chicken is Great!")
+elif (food == "noodles") or (food == "Noodles"):
+    print("Noodles are delicious!")
+elif (food == "soup") or (food == "Soup"):
+    print("Soup is nice and hot!")
+elif (food == "beef") or (food =="Beef"):
+    print("Beef is very yummy!")
+else:
+    print("Your choice would also be great to have!")
+
+

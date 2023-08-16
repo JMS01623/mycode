df15f8c04554bde586d9818c00af5b95f919ad62
JMS01623 <michaeljustinc@gmail.com>
2023-08-16 21:56:26 +0000

    Playing with python pip packages

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+my_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+# display the first item in the list
+print(f" {my_list[0]}" )
+
+# display the second item in the list
+print(f"The second item in the list (port):", my_list[1] )
+
+# display the third item in the list
+print(f"The last item in the list (state):", my_list[2] )
+

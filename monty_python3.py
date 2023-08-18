8852e2b6047c73994c01e4b1e8cd8c34ecd22ad9
JMS01623 <michaeljustinc@gmail.com>
2023-08-18 21:14:00 +0000

    Playing with python pip packages

--- monty_python3.py
+++ monty_python3.py
@@ -5,10 +5,10 @@
 round = 0
 answer = " "
 
-while round < 3 and answer != "Brian":
+while round < 3 and answer != "brian":
     round += 1     # increase the round counter by 1
-    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    if answer == "Brian": # logic to check if user gave correct answer
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ').lower()
+    if answer.lower() == "Brian": # logic to check if user gave correct answer
         print("Correct!")
     elif round == 3:    # logic to ensure round has not yet reached 3
         print("Sorry, the answer was Brian.")
